bbc9838c576eccdadd359b02b7501febe2595d96
Yoann <ybuzenet@gmail.com>
2024-01-25 19:04:14 +0100

    searching with embedding is huge

--- search-tutorial/app.py
+++ search-tutorial/app.py
@@ -18,28 +18,12 @@ def handle_search():
     filters, parsed_query = extract_filters(query)
     from_ = request.form.get('from_', type=int, default=0)
 
-    if parsed_query:
-        search_query = {
-            'must': {
-                'multi_match': {
-                    'query': parsed_query,
-                    'fields': ['name', 'summary', 'content'],
-                }
-            }
-        }
-    else:
-        search_query = {
-            'must': {
-                'match_all': {}
-            }
-        }
-
     results = es.search(
-        query={
-            'bool': {
-                **search_query,
-                **filters
-            }
+        knn={
+            'field': 'embedding',
+            'query_vector': es.get_embedding(parsed_query),
+            'num_candidates': 50,
+            'k': 10,
         }, size=5, from_=from_
     )
     return render_template('index.html', results=results['hits']['hits'],
--- search-tutorial/search.py
+++ search-tutorial/search.py
@@ -5,12 +5,14 @@ import time
 
 from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
+from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
 
 class Search:
     def __init__(self):
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
         self.es = Elasticsearch('http://localhost:9200',timeout=150)
         client_info = self.es.info()
         print('Connected to Elasticsearch!')
@@ -19,21 +21,31 @@ class Search:
     def create_index(self):
         print("okay buddy1")
         self.es.indices.delete(index='my_documents', ignore_unavailable=True)
-        self.es.indices.create(index='my_documents')  
+        self.es.indices.create(index='my_documents', mappings={
+            'properties': {
+                'embedding': {
+                    'type': 'dense_vector',
+                }
+            }
+        })    
         print("okay buddy1.5")
 
 
     def insert_document(self, document):
-        return self.es.index(index='my_documents', body=document)    
-    
+        return self.es.index(index='my_documents', document={
+            **document,
+            'embedding': self.get_embedding(document['summary']),
+        })
+
     def insert_documents(self, documents):
         operations = []
         for document in documents:
             operations.append({'index': {'_index': 'my_documents'}})
-            operations.append(document)
-            print("okay buddy20")
+            operations.append({
+                **document,
+                'embedding': self.get_embedding(document['summary']),
+            })
         print("okay buddy30")
-
         return self.es.bulk(operations=operations)
     
     def reindex(self):
@@ -50,3 +62,6 @@ class Search:
     
     def retrieve_document(self, id):
         return self.es.get(index='my_documents', id=id)
+    
+    def get_embedding(self, text):
+        return self.model.encode(text)
